192b6745e2c453602762440e517a8e27dca7b0b4
lucasGrt <lucasrigou@uol.com.br>
2024-06-28 11:11:05 -0300

    criado tela sim

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Button } from "./components/button"
+import { Ondas } from "./components/ondas"
+function App() {
+
+  return (
+    <div className="relative overflow-hidden">
+      <Ondas/>
+      <div className="bg-gray-300 w-screen h-screen flex flex-col items-center justify-center px-4 overflow-hidden" >
+        <div className="z-10">
+        <h1 className=" text-4xl font-bold flex justify-center items-center max-sm:text-center">Lorem ipsum dolor ?</h1>
+          <div className="flex flex-row w-full items-center justify-center gap-6 mt-10 max-sm:flex-col max-sm:gap-2">
+
+            <Button text="SIM" color="green" onClick={() => {}}  />
+            <Button id="botaoNao"  text="NÃO" color="red" onClick={() => {}}  />
+            
+            
+          </div>
+        </div>
+      </div>
+    
+    </div>
+  )
+}
+
+export default App
--- src/routes/Sim.tsx
+++ src/routes/Sim.tsx
@@ -1,7 +1,39 @@
+import { Ondas } from "../components/ondas"
+import pedroAudio from "../assets/pedro-pedro-pe.mp3"
+import pedroGif from "../assets/pedroGif.gif"
+import {useRef,useEffect} from "react"
+
 function Sim() {
 
+    const audioref = useRef<HTMLAudioElement>(null);
+    useEffect(() => {
+        if (audioref.current)
+            audioref.current.play()
+    },[])
+
     return (
-     <h1>olá</h1>
+        <div className="relative overflow-hidden">
+        <Ondas/>
+            <div className="w-screen h-screen">
+                <audio ref={audioref}> <source src={pedroAudio} type="audio/mp3"/></audio>
+                <div className="w-screen h-screen items-center justify-center flex flex-col">
+        <div className="size-64 rounded-full overflow-hidden flex items-center justify-center">
+          <img src={pedroGif} alt="pedro gif" className="size-[110%]" />
+        </div>
+
+        <h1 className="text-slate-700 text-3xl font-semibold text-center z-10 mt-20">
+          😉 Sint consectetur fugiat ut eu aliquip. 😏
+        </h1>
+
+        <p className="text-slate-600 text-base font-thin mt-6 w-2/4 z-10">
+          Elit ex eiusmod elit occaecat nostrud qui magna ullamco quis proident
+          ea ad. Nisi ut irure minim officia incididunt est quis excepteur
+          dolor. Qui nisi nisi aute occaecat eiusmod ea et.
+        </p>
+      </div>
+            </div>
+            
+        </div>
     )
   }
   
